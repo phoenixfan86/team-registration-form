70c339adfe74836a7f71128cc4338e4f774b6eaa
phoenixfan86 <phoenixfan86@gmail.com>
2025-03-10 17:55:21 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Newmember from './components/Newmember'
 function App() {
   return (
     <section className="">
-      <div className="w-full h-screen flex items-center justify-center bg-[url(./img/bg_1.png)] bg-cover">
+      <div className="w-full h-screen flex items-center justify-center bg-[url(/img/bg_1.png)] bg-cover">
         <Newmember />
       </div>
     </section>
